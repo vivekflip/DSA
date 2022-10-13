bac9f1bd27b53759e2791f6a32e71e6bc9acba49
cypher900vs <114932554+cypher900vs@users.noreply.github.com>
2022-10-14 01:39:05 +0530

    note
    note

---
+++ Note.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+// import { Button } from '@mui/material';
+
+const Note=(props)=>{ 
+
+  const deleteNote=()=>{
+    props.deleteItem(props.id);
+  }
+
+  return (
+    <>
+  <div className='note'>
+  
+  <h1>{props.title}</h1>
+  <br/>
+  <p>{props.content} </p>
+  <button className='btn' onClick={deleteNote}>
+  <DeleteOutlineOutlinedIcon className='deleteIcon'/>
+  
+  </button>
+  </div>
+ 
+    </>
+  )
+};
+
+export default Note;
