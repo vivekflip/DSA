2a0db82c7d16804af8e07cdd8df949ee26c495fc
Parag <paragagrawal477@gmail.com>
2022-10-22 23:48:18 +0530

    Added a new project

--- website_blocker.py
+++ website_blocker.py
@@ -1,6 +1,3 @@
-# """This is a simple website blocker project implemented in Python, It can be used to block certain websites during working time to reduce distraction thus improving productivity"""
-
-
 import time
 from datetime import datetime as dt
 
